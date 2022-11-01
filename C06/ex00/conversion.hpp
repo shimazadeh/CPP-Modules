d3467f48be0cfbf6487bc83a3ef3fe52acbfe0de
Adrien <adrien.guillard@gmail.com>
2022-11-01 01:32:16 +0100

    C06 in progress

--- C06/ex00/conversion.hpp
+++ C06/ex00/conversion.hpp
@@ -3,4 +3,23 @@
 
 #include <iostream>
 
+class type
+{
+    public:
+        type(void);
+        type(char *str);
+        ~type(void);
+
+    void    print_type(void);
+    void    if_digit(void);
+    void    if_valid(void);
+    
+    private:
+        char    *str;
+        int     num_i;
+        double  num_d;
+        float   num_f;
+        char    c;
+
+};
 #endif
